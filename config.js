34d62d434892bba994696c15d05ca47cda472cc6
enzie15 <109117149+enzie15@users.noreply.github.com>
2024-11-07 09:05:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1Z2hTnmEvWUJTKpZqdW9Dr6-q2tP46TGNnCLbXA1zqNk";
 
 
 /* ------------------------------------------------------------------- */
